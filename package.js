2470fe71be7ac6f111a71886d60f09e52e8b212e
Jeremie Parker <jparker@guimiesoft.com>
2014-09-08 17:35:16 +0200

    Update for Meteor 0.9.x new package system

--- package.js
+++ package.js
@@ -1,14 +1,18 @@
 Package.describe({
-  summary: "Base64 (en/de)coding for CryptoJS, standard secure crypto algorithms"
+	summary: 'Base64 (en/de)coding for CryptoJS, standard secure crypto algorithms',
+	version: '3.1.2-1',
+	git: 'https://github.com/p-j/meteor-crypto-base64.git'
 });
 
 Package.on_use(function (api, where) {
-  api.use('crypto-base', ['client', 'server']);
-  api.add_files('enc-base64.js', ['client', 'server']);
+	api.versionsFrom('METEOR@0.9.0');
+	api.use('jparker:meteor-crypto-base@3.1.2-1', ['client', 'server']);
+	api.imply('jparker:meteor-crypto-base', ['client', 'server']);
+	api.add_files('enc-base64.js', ['client', 'server']);
 });
 
 Package.on_test(function (api) {
-  api.use(['crypto-base', 'crypto-base64', 'tinytest']);
+	api.use(['jparker:meteor-crypto-base', 'jparker:meteor-crypto-base64', 'tinytest']);
 
-  api.add_files('crypto-base64_tests.js', ['client', 'server']);
+	api.add_files('crypto-base64_tests.js', ['client', 'server']);
 });
